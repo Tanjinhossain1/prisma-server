2e03637d10cd6aef53d48937f5d69cda74a2ddec
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-08-30 23:46:41 +0600

    add student semester and semester registration update delete

---
+++ prisma/migrations/20230830173912_student_semester/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "WeekDays" AS ENUM ('SATURDAY', 'SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY');
+
+-- CreateTable
+CREATE TABLE "student_semester_registrations" (
+    "id" TEXT NOT NULL,
+    "isConfirmed" BOOLEAN DEFAULT false,
+    "totalCreditsTaken" INTEGER DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "semesterRegistrationId" TEXT NOT NULL,
+
+    CONSTRAINT "student_semester_registrations_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registrations" ADD CONSTRAINT "student_semester_registrations_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registrations" ADD CONSTRAINT "student_semester_registrations_semesterRegistrationId_fkey" FOREIGN KEY ("semesterRegistrationId") REFERENCES "semester_registration"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
